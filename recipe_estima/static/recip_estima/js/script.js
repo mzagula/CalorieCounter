49bd9eb283486b053aab79dc5d24a6152e6d25d5
Marta Zaguła <mzagula1992@gmail.com>
2023-05-27 23:36:24 +0200

    #0018 add edit option to detail

--- recipe_estima/static/recip_estima/js/script.js
+++ recipe_estima/static/recip_estima/js/script.js
@@ -10,3 +10,13 @@ function hideConfirmationDialog() {
 function deleteRecipe(recipeId) {
     window.location.href = `/recip_estima/recipe_delete/${recipeId}`;
 }
+
+function enableEditMode() {
+    var displayField = document.getElementById("display-field");
+    displayField.style.display = "none";
+
+    var editField = document.getElementById("edit-field");
+    editField.style.display = "inline";
+    editField.value = displayField.innerText;
+    editField.focus();
+}
