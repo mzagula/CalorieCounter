963bbb132bea545db0c83adc00399d6355205887
Marta Zaguła <mzagula1992@gmail.com>
2023-05-28 23:14:36 +0200

    #0020 edit while doubleclick on field

--- recipe_estima/static/recip_estima/js/script.js
+++ recipe_estima/static/recip_estima/js/script.js
@@ -11,12 +11,21 @@ function deleteRecipe(recipeId) {
     window.location.href = `/recip_estima/recipe_delete/${recipeId}`;
 }
 
-function enableEditMode() {
-    var displayField = document.getElementById("display-field");
-    displayField.style.display = "none";
+    function enableEditMode() {
+        var field = document.getElementById("editable-field");
+        var fieldValue = field.innerText;
 
-    var editField = document.getElementById("edit-field");
-    editField.style.display = "inline";
-    editField.value = displayField.innerText;
-    editField.focus();
-}
+        var inputField = document.createElement("input");
+        inputField.type = "text";
+        inputField.value = fieldValue;
+
+        inputField.onblur = function() {
+            var updatedValue = inputField.value;
+            field.innerText = updatedValue;
+            field.parentNode.removeChild(inputField);
+        };
+
+        field.innerText = "";
+        field.appendChild(inputField);
+        inputField.focus();
+    }
